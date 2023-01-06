6b10730341ee1dc9bca4258073bacdee79813ab2
hoatranobita <86996836+hoatranobita@users.noreply.github.com>
2023-01-06 14:05:10 +0700

    Add files via upload

--- apps/general.py
+++ apps/general.py
@@ -269,10 +269,104 @@ layout = dbc.Container([
                                 color='rgba(50, 171, 96, 0.6)', type='dot')
                 ])
             ])
-        ], xs=12, lg=3, md=6)
-
+        ], xs=12, lg=3, md=6),
+    ], className='p-2 align-items-center'),
+    dbc.Row([
+        dbc.Col([
+            html.H6('5.1 OnlineBackup Situation', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_17', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('5.2 Churn by OnlineBackup', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_18', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('6.1 DeviceProtection Situation', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_19', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('6.2 Churn by DeviceProtection', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_20', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+    ], className='p-2 align-items-center'),
+    dbc.Row([
+        dbc.Col([
+            html.H6('7.1 TechSupport Situation', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_21', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('7.2 Churn by TechSupport', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_22', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('8.1 StreamingTV Situation', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_23', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('8.2 Churn by StreamingTV', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_24', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+    ], className='p-2 align-items-center'),
+    dbc.Row([
+        dbc.Col([
+            html.H6('9.1 StreamingMovies Situation', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_25', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
+        dbc.Col([
+            html.H6('9.2 Churn by StreamingMovies', style={'text-align': 'left'}),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Loading(children=[dcc.Graph(figure={}, id='figure_26', style={'height': 350})],
+                                color='rgba(50, 171, 96, 0.6)', type='dot')
+                ])
+            ])
+        ], xs=12, lg=3, md=6),
     ], className='p-2 align-items-center')
-
 ], fluid=True)
 
 
@@ -310,7 +404,17 @@ layout = dbc.Container([
                Output("figure_13", "figure"),
                Output("figure_14", "figure"),
                Output("figure_15", "figure"),
-               Output("figure_16", "figure")],
+               Output("figure_16", "figure"),
+               Output("figure_17", "figure"),
+               Output("figure_18", "figure"),
+               Output("figure_19", "figure"),
+               Output("figure_20", "figure"),
+               Output("figure_21", "figure"),
+               Output("figure_22", "figure"),
+               Output("figure_23", "figure"),
+               Output("figure_24", "figure"),
+               Output("figure_25", "figure"),
+               Output("figure_26", "figure")],
               [Input("radioitems-input", "value"),
                Input(ThemeSwitchAIO.ids.switch("theme"), "value")])
 def update_graph(radio, theme):
@@ -653,6 +757,184 @@ def update_graph(radio, theme):
         fig_16.update_yaxes(showline=False, showgrid=False, exponentformat="none")
         fig_16.update_xaxes(showline=False, showgrid=False, exponentformat="none")
 
+        df15 = df.groupby(['OnlineBackup', radio])[radio].count().reset_index(name='counts')
+        fig_17 = px.bar(df15, x=radio, y='counts', color='OnlineBackup',
+                       color_discrete_sequence=px.colors.qualitative.Safe, text='counts', barmode='group')
+        fig_17.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_17.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_17.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_17.update_traces(width=0.25, texttemplate='%{text:,.0f}', textposition='inside')
+        df16 = df.groupby(['OnlineBackup', radio, 'Churn'])[radio].count().reset_index(name='counts')
+        df16_1 = df16[df16[radio] == z1]
+        df16_2 = df16[df16[radio] == z2]
+
+        fig_18 = go.Figure()
+        fig_18.add_trace(go.Bar(name=z3,x=[tuple(df16_1['Churn']), tuple(df16_1['OnlineBackup'])],
+                               y=list(df16_1['counts'])),
+                        )
+        fig_18.add_trace(go.Bar(name=z4,x=[tuple(df16_2['Churn']), tuple(df16_2['OnlineBackup'])],
+                               y=list(df16_2['counts'])),
+                        )
+        fig_18.update_layout(barmode='stack')
+        fig_18.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_18.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_18.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+
+
+        df17 = df.groupby(['DeviceProtection', radio])[radio].count().reset_index(name='counts')
+        fig_19 = px.bar(df17, x=radio, y='counts', color='DeviceProtection',
+                       color_discrete_sequence=px.colors.qualitative.Safe, text='counts', barmode='group')
+        fig_19.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_19.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_19.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_19.update_traces(width=0.25, texttemplate='%{text:,.0f}', textposition='inside')
+        df18 = df.groupby(['DeviceProtection', radio, 'Churn'])[radio].count().reset_index(name='counts')
+        df18_1 = df18[df18[radio] == z1]
+        df18_2 = df18[df18[radio] == z2]
+
+        fig_20 = go.Figure()
+        fig_20.add_trace(go.Bar(name=z3,x=[tuple(df18_1['Churn']), tuple(df18_1['DeviceProtection'])],
+                               y=list(df18_1['counts'])),
+                        )
+        fig_20.add_trace(go.Bar(name=z4,x=[tuple(df18_2['Churn']), tuple(df18_2['DeviceProtection'])],
+                               y=list(df18_2['counts'])),
+                        )
+        fig_20.update_layout(barmode='stack')
+        fig_20.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_20.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_20.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+
+
+        df19 = df.groupby(['TechSupport', radio])[radio].count().reset_index(name='counts')
+        fig_21 = px.bar(df19, x=radio, y='counts', color='TechSupport',
+                       color_discrete_sequence=px.colors.qualitative.Safe, text='counts', barmode='group')
+        fig_21.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_21.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_21.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_21.update_traces(width=0.25, texttemplate='%{text:,.0f}', textposition='inside')
+        df20 = df.groupby(['TechSupport', radio, 'Churn'])[radio].count().reset_index(name='counts')
+        df20_1 = df20[df20[radio] == z1]
+        df20_2 = df20[df20[radio] == z2]
+
+        fig_22 = go.Figure()
+        fig_22.add_trace(go.Bar(name=z3,x=[tuple(df20_1['Churn']), tuple(df20_1['TechSupport'])],
+                               y=list(df20_1['counts'])),
+                        )
+        fig_22.add_trace(go.Bar(name=z4,x=[tuple(df20_2['Churn']), tuple(df20_2['TechSupport'])],
+                               y=list(df20_2['counts'])),
+                        )
+        fig_22.update_layout(barmode='stack')
+        fig_22.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_22.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_22.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+
+        df21 = df.groupby(['StreamingTV', radio])[radio].count().reset_index(name='counts')
+        fig_23 = px.bar(df21, x=radio, y='counts', color='StreamingTV',
+                       color_discrete_sequence=px.colors.qualitative.Safe, text='counts', barmode='group')
+        fig_23.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_23.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_23.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_23.update_traces(width=0.25, texttemplate='%{text:,.0f}', textposition='inside')
+        df22 = df.groupby(['StreamingTV', radio, 'Churn'])[radio].count().reset_index(name='counts')
+        df22_1 = df22[df22[radio] == z1]
+        df22_2 = df22[df22[radio] == z2]
+
+        fig_24 = go.Figure()
+        fig_24.add_trace(go.Bar(name=z3,x=[tuple(df22_1['Churn']), tuple(df22_1['StreamingTV'])],
+                               y=list(df22_1['counts'])),
+                        )
+        fig_24.add_trace(go.Bar(name=z4,x=[tuple(df22_2['Churn']), tuple(df22_2['StreamingTV'])],
+                               y=list(df22_2['counts'])),
+                        )
+        fig_24.update_layout(barmode='stack')
+        fig_24.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_24.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_24.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+
+        df23 = df.groupby(['StreamingMovies', radio])[radio].count().reset_index(name='counts')
+        fig_25 = px.bar(df23, x=radio, y='counts', color='StreamingMovies',
+                       color_discrete_sequence=px.colors.qualitative.Safe, text='counts', barmode='group')
+        fig_25.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_25.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_25.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_25.update_traces(width=0.25, texttemplate='%{text:,.0f}', textposition='inside')
+        df24 = df.groupby(['StreamingMovies', radio, 'Churn'])[radio].count().reset_index(name='counts')
+        df24_1 = df24[df24[radio] == z1]
+        df24_2 = df24[df24[radio] == z2]
+
+        fig_26 = go.Figure()
+        fig_26.add_trace(go.Bar(name=z3,x=[tuple(df24_1['Churn']), tuple(df24_1['StreamingMovies'])],
+                               y=list(df24_1['counts'])),
+                        )
+        fig_26.add_trace(go.Bar(name=z4,x=[tuple(df24_2['Churn']), tuple(df24_2['StreamingMovies'])],
+                               y=list(df24_2['counts'])),
+                        )
+        fig_26.update_layout(barmode='stack')
+        fig_26.update_layout(template=template, legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+        fig_26.update_yaxes(showline=False, showgrid=False, exponentformat="none")
+        fig_26.update_xaxes(showline=False, showgrid=False, exponentformat="none")
+
+
         return html.H6(f'Customer amount by {radio}', style={'color': '#7172ba'}), \
                html.H6(f'{radio} {customer_type_1}: {count_1}'), \
                html.H6(f'{radio} {customer_type_2}: {count_2}'), \
@@ -687,7 +969,8 @@ def update_graph(radio, theme):
                html.Div([html.Span(f'{radio} {customer_type_17} churn Yes median total charge is $ {median_5}'), \
                          html.Span(f'{radio} {customer_type_18} churn Yes median total charge is $ {median_6}')],
                         className="d-grid"),\
-               fig, fig_2, fig_3, fig_4, fig_5, fig_6, fig_7, fig_8, fig_9, fig_10, fig_11, fig_12, fig_13, fig_14, fig_15, fig_16
+               fig, fig_2, fig_3, fig_4, fig_5, fig_6, fig_7, fig_8, fig_9, fig_10, fig_11, fig_12, fig_13, \
+               fig_14, fig_15, fig_16, fig_17, fig_18, fig_19, fig_20, fig_21, fig_22, fig_23, fig_24, fig_25, fig_26
 
     else:
         raise PreventUpdate
