7c6521fe5ae5dcbfe88536a0a48c5b339694f523
hoatranobita <86996836+hoatranobita@users.noreply.github.com>
2023-01-09 15:24:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = dash.Dash(__name__,
                 suppress_callback_exceptions=True,
                 meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
                 external_stylesheets=[dbc.themes.LUX,FA,FA2,dbc_css],
-                title='Iowa Sales Report'
+                title='Holiday Challenge'
                 )
 
 server = app.server
@@ -58,7 +58,7 @@ def render_page_content(pathname):
     elif pathname == "/page-5":
         return about.layout
 if __name__ == '__main__':
-    app.run_server(debug=True,port='8056')
+    app.run_server(debug=True)
 
 
 
